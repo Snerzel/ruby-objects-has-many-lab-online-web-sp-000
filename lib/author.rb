1311859f83d8122646697e6dbb7cb0d28c84a2f0
Cherissa Woudenberg <snerzel@comcast.net>
2021-04-21 03:14:33 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -15,4 +15,13 @@ def add_post(post)
   post.auther = self
 end
 
+def add_post_by_title(title)
+  author = Post.new(title)
+  add_post(author)
+end
+
+def self.post_count
+  Post.all.count
+end
+
 end
